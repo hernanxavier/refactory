ae1eac8a82ffcbc3972b391d8f11aa410e94910b
H. Xavier Pacheco <hernanxavierp@gmail.com>
2014-07-02 17:57:15 -0500

    Modulo helpdesk Agregado

--- iaen_base/iaen_base.py
+++ iaen_base/iaen_base.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+# #############################################################################
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
@@ -52,34 +52,36 @@ class ethnic_group(osv.osv):
     }
 
 
-
 class zones(osv.osv):
     _name = "zones"
     _description = "Categoriza zonas por provincias"
     _order = "name"
     _sql_constraints = [('name_unique', 'unique(name)', 'Ya existe una zona con el mismo nombre')]
     _columns = {
-        "name": fields.char("Zona",size=10, required=True),
-        "description": fields.text("Descripción",help="Descripción para la zona."),
-        "country_id": fields.many2one("res.country","Pais",required=True),
-        }
+        "name": fields.char("Zona", size=10, required=True),
+        "description": fields.text("Descripción", help="Descripción para la zona."),
+        "country_id": fields.many2one("res.country", "Pais", required=True),
+    }
+
 
 class res_country_state(osv.osv):
     _inherit = "res.country.state"
     _columns = {
-        "zone_id": fields.many2one("zones","Zona",help="Distribución zonal, a la que pertenece la provincia")
+        "zone_id": fields.many2one("zones", "Zona", help="Distribución zonal, a la que pertenece la provincia")
     }
-    def change_zone_id(self,cr,uid,ids,zone):
+
+    def change_zone_id(self, cr, uid, ids, zone):
         if zone:
-            zone_obj = self.pool.get('zones').browse(cr,uid,zone)
+            zone_obj = self.pool.get('zones').browse(cr, uid, zone)
             print zone_obj
-            country_obj = self.pool.get('res.country').browse(cr,uid,zone_obj.country_id.id)
+            country_obj = self.pool.get('res.country').browse(cr, uid, zone_obj.country_id.id)
             print country_obj.id
             #self.write(cr,uid,ids,{'country_id':country_obj[0]})
-            return {'value': {'country_id':country_obj.id}}
+            return {'value': {'country_id': country_obj.id}}
         else:
             return False
-        
+
+
 #    _order = "name"
 #    _sql_constraints = [('name_unique', 'unique(name)', 'Ya existe una zona con el mismo nombre')]
 
@@ -89,9 +91,10 @@ class canton(osv.osv):
     _order = "name"
     _columns = {
         "name": fields.char("Ciudad/Cantón", size=15, required=True),
-        "country_id": fields.many2one("res.country.state","Provincia",required=True),
+        "country_id": fields.many2one("res.country.state", "Provincia", required=True),
         "description": fields.text("Descripción"),
     }
+
 
 #class city(osv.osv):
 #    _name = "city"
@@ -108,22 +111,23 @@ class parish(osv.osv):
     _order = "name"
     _columns = {
         "name": fields.char("Parroquia", size=15, required=True),
-        "canton_id": fields.many2one("canton","Cantón",required=True),
+        "canton_id": fields.many2one("canton", "Cantón", required=True),
         "description": fields.text("Descripción"),
     }
 
 
 class blood_type(osv.osv):
-	_name = "blood_type"
-	_description = "Registra los tipos de sangre"
-	_columns = {
-		'name': fields.char("Nombre", size=3, required=True),
-	}
-	_order = "name"
-	_sql_constraints = [('name_unique', 'unique(name)', _(u'Ya existe un Tipo de Sangre con ese nombre.'))]
-	def _no_numbers(self, cr, uid, ids):
-		for bloody_type in self.browse(cr, uid, ids):
-			if re.search("[0-9]", bloody_type.name): return False
-		return True 
-	_constraints = [(_no_numbers, _(u"El Tipo de Sangre no debe contener números."), ['name'])]
+    _name = "blood_type"
+    _description = "Registra los tipos de sangre"
+    _columns = {
+    'name': fields.char("Nombre", size=3, required=True),
+    }
+    _order = "name"
+    _sql_constraints = [('name_unique', 'unique(name)', _(u'Ya existe un Tipo de Sangre con ese nombre.'))]
 
+    def _no_numbers(self, cr, uid, ids):
+        for bloody_type in self.browse(cr, uid, ids):
+            if re.search("[0-9]", bloody_type.name): return False
+        return True
+
+    _constraints = [(_no_numbers, _(u"El Tipo de Sangre no debe contener números."), ['name'])]
